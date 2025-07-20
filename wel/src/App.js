ac2cb749368f7fc87c47321940cbc43ea5efe34d
Dmitry_Gaan <verefer.asura@gmail.com>
2025-07-20 20:54:46 +0600

    Создал шапку header

--- wel/src/App.js
+++ wel/src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 function App() {
   return (
     <header>
-
+      <img src={logo} className="logo-header" alt="logo" />
+      <a href='#'>Меню 1</a>
+      <a href='#'>Меню 2</a>
+      <a href='#'>Меню 3</a>
+      <a href='#'>Меню 4</a>
+      <a href='#'>Меню 5</a>
+      <input type='search' placeholder='Поиск'/>
+      <a href='#'>Вход</a>
+      <a className="reg-btn" href='#'>Регистрация</a>
     </header>
   );
 }
