cd3711d7fae6c69d7ac23c2069ddf966925482f4
Dmitry_Gaan <verefer.asura@gmail.com>
2025-07-21 10:11:08 +0600

    create php structure code 	modified:   wel/public/index.html 	new file:   welphp/assets/img/favicon.ico 	new file:   welphp/assets/img/logo.svg 	new file:   welphp/assets/img/logo192.png 	new file:   welphp/assets/img/logo512.png 	new file:   welphp/assets/script.js 	new file:   welphp/assets/style.css 	new file:   welphp/index.php 	modified:   "\320\224\320\270\320\267\320\260\320\271\320\2751.psd"

---
+++ welphp/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="assets/img/favicon.ico" >
+    <meta name="theme-color" content="#000000" >
+    <meta
+      name="description"
+      content="Web site Web Elements Lab">
+    <link rel="apple-touch-icon" href="assets/img/logo192.png" >
+    <title>Web Elements Lab</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <header>
+        <div class="nav-left">
+            <img src=assets/img/logo.svg class="logo-header" alt="logo">
+            <a href='#'>Меню 1</a>
+            <a href='#'>Меню 2</a>
+            <a href='#'>Меню 3</a>
+            <a href='#'>Меню 4</a>
+            <a href='#'>Меню 5</a>
+        </div>
+        <div class="nav-right">
+            <input type='search' placeholder='Поиск...'>
+            <a href='#'>Вход</a>
+            <a class="reg-btn" href='#'>Регистрация</a>
+        </div>
+    </header>
+</body>
+</html>
