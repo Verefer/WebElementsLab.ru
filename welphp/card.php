f7dd9bdbf131292da7603a0f101f41d78235c72d
Dmitry_Gaan <verefer.asura@gmail.com>
2025-07-22 16:47:42 +0600

    Структура для карточки 	new file:   welphp/card.php 	modified:   welphp/index.php

---
+++ welphp/card.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="assets/img/favicon.ico" >
+    <meta name="theme-color" content="#000000" >
+    <meta
+      name="description"
+      content="content">
+    <link rel="apple-touch-icon" href="assets/img/logo192.png" >
+    <title>Вход | WebElementsLab</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+<div class="wrapper">
+<?php require_once 'includes/header.php'; ?>
+<main>
+    <div class="d-flex gap1">
+        <div>
+            <p>Название</p>
+            <p>Автор</p>
+        </div>
+        <div>
+            <div>
+                <div>
+                    <p>Элемент SVG или как html css</p>
+                </div>
+                <div>
+                    <a class="btn-card" href="#">Избранное</a>
+                    <a class="btn-card" href="#">Подписаться</a>
+                </div>
+            </div>
+            <div>
+                <p>Сюда как-то реализовать код</p>
+            </div>
+        </div>
+        <div>
+            <p>Короткая информация с ссылками на то как подключать стили</p>
+        </div>
+    </div>
+</main>
+<?php require_once 'includes/footer.php'; ?>
+</div>
+</body>
+</html>
