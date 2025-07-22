fd88c4b7726180ca85a9fc4b50ffb80d3c371c49
Dmitry_Gaan <verefer.asura@gmail.com>
2025-07-22 09:21:38 +0600

    Удаление react кода 	deleted:    wel/.gitignore 	deleted:    wel/README.md 	deleted:    wel/package-lock.json 	deleted:    wel/package.json 	deleted:    wel/public/favicon.ico 	deleted:    wel/public/index.html 	deleted:    wel/public/logo192.png 	deleted:    wel/public/logo512.png 	deleted:    wel/public/manifest.json 	deleted:    wel/public/robots.txt 	deleted:    wel/src/App.css 	deleted:    wel/src/App.js 	deleted:    wel/src/App.test.js 	deleted:    wel/src/index.css 	deleted:    wel/src/index.js 	deleted:    wel/src/logo.svg 	deleted:    wel/src/reportWebVitals.js 	deleted:    wel/src/setupTests.js 	modified:   welphp/includes/header.php 	modified:   welphp/index.php

---
+++ welphp/includes/header.php
@@ -0,0 +1,15 @@
+<header>
+        <div class="nav-left">
+            <img src=assets/img/logo.svg class="logo-header" alt="logo">
+            <a href='#'>Меню 1</a>
+            <a href='#'>Меню 2</a>
+            <a href='#'>Меню 3</a>
+            <a href='#'>Меню 4</a>
+            <a href='#'>Меню 5</a>
+        </div>
+        <div class="nav-right">
+            <!-- <input type='search' placeholder='Поиск...'> -->
+            <a href='login.php'>Вход</a>
+            <a class="reg-btn anim-hover-box-shadow" href='register.php'>Регистрация</a>
+        </div>
+    </header>
--- welphp/index.php
+++ welphp/index.php
@@ -13,21 +13,7 @@
     <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
-    <header>
-        <div class="nav-left">
-            <img src=assets/img/logo.svg class="logo-header" alt="logo">
-            <a href='#'>Меню 1</a>
-            <a href='#'>Меню 2</a>
-            <a href='#'>Меню 3</a>
-            <a href='#'>Меню 4</a>
-            <a href='#'>Меню 5</a>
-        </div>
-        <div class="nav-right">
-            <!-- <input type='search' placeholder='Поиск...'> -->
-            <a href='login.php'>Вход</a>
-            <a class="reg-btn anim-hover-box-shadow" href='register.php'>Регистрация</a>
-        </div>
-    </header>
+    <?php require_once 'includes/header.php'; ?>
     <div class="wrapper">
         <main>
         <div class="slogan">
