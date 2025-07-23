d813a89bab565adafa01b3ba84539d12375f55b2
Dmitry_Gaan <verefer.asura@gmail.com>
2025-07-23 10:49:58 +0600

    edit errors

--- includes/register.php
+++ includes/register.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = trim($_POST['username'] ?? '');
-    $email = trim($_POST['login'] ?? '');
+    $email = trim($_POST['email'] ?? '');
     $password = $_POST['password'] ?? '';
     $agree_privacy = $_POST['agree-privacy'] ?? '';
     $agree_mailing = $_POST['agree-mailing'] ?? '';
