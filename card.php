c56b787ae69916948bdd618008ce96a9c4109743
Dmitry_Gaan <verefer.asura@gmail.com>
2025-07-24 08:12:27 +0600

    3 Карточка Стили поплыли

--- card.php
+++ card.php
@@ -22,38 +22,36 @@
             <h2>Автор</h2>
         </div>
         <div class="f-d-row d-flex gap1">
+            <!-- Preview -->
             <div class="left-card-page d-flex gap1 f-d-column">
-                <div class="f-d-row d-flex gap1">
-                <!-- Preview -->
-                <div class="left-card-page d-flex gap1 f-d-column">
-                    <div class="block-element d-flex j-c-center a-i-center">
-                    <a class="btn" href="#">BUTTON</a> <!-- Живой результат -->
-                    </div>
-                    <div class="d-flex gap1 f-d-column">
-                    <a class="btn-card j-c-center d-flex" href="#">Избранное</a>
-                    <a class="btn-card j-c-center d-flex" href="#">Подписаться</a>
-                    </div>
+                <div class="block-element d-flex j-c-center a-i-center">
+                <a class="btn" href="#">BUTTON</a> <!-- Живой результат -->
+                </div>
+                <div class="d-flex gap1 f-d-column">
+                <a class="btn-card j-c-center d-flex" href="#">Избранное</a>
+                <a class="btn-card j-c-center d-flex" href="#">Подписаться</a>
+                </div>
+            </div>
+
+            <!-- Code Tabs -->
+            <div class="block-code">
+                <div class="tabs d-flex gap05">
+                <button class="tab-btn active" data-tab="html">HTML</button>
+                <button class="tab-btn" data-tab="css">CSS</button>
+                <button class="tab-btn" data-tab="js">JS</button>
                 </div>
 
-                <!-- Code Tabs -->
-                <div class="block-code">
-                    <div class="tabs d-flex gap05">
-                    <button class="tab-btn active" data-tab="html">HTML</button>
-                    <button class="tab-btn" data-tab="css">CSS</button>
-                    <button class="tab-btn" data-tab="js">JS</button>
-                    </div>
-
-                    <div class="tab-content active" id="html">
-                    <pre><code class="language-html">&lt;a class="btn" href="#"&gt;BUTTON&lt;/a&gt;</code></pre>
-                    </div>
-                    <div class="tab-content" id="css">
-                    <pre><code class="language-css">.btn { background: #000; color: #fff; padding: 0.5em 1em; }</code></pre>
-                    </div>
-                    <div class="tab-content" id="js">
-                    <pre><code class="language-js">document.querySelector('.btn').onclick = () => alert('Нажато');</code></pre>
-                    </div>
+                <div class="tab-content active" id="html">
+                <pre><code class="language-html">&lt;a class="btn" href="#"&gt;BUTTON&lt;/a&gt;</code></pre>
                 </div>
+                <div class="tab-content" id="css">
+                <pre><code class="language-css">.btn { background: #000; color: #fff; padding: 0.5em 1em; }</code></pre>
                 </div>
+                <div class="tab-content" id="js">
+                <pre><code class="language-js">document.querySelector('.btn').onclick = () => alert('Нажато');</code></pre>
+                </div>
+            </div>
+            </div>
 
 
         </div>
